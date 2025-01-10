b77bc089baebebe8dc3f8927898444c7d74aca08
gabistam <gabistam@gmail.com>
2025-01-10 19:37:20 +0100

    Add user model and authentication routes

---
+++ models/User.js
@@ -0,0 +1,31 @@
+// models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Middleware Mongoose pour hasher le mot de passe avant sauvegarde
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Méthode pour comparer un mot de passe
+userSchema.methods.comparePassword = async function (plainPassword) {
+  return bcrypt.compare(plainPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+// routes/auth.js
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+
+// Inscription
+router.post('/signup', authController.signup);
+
+// Connexion
+router.post('/login', authController.login);
+
+module.exports = router;
